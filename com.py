bbc234eaf076eb9ce3ea2d25b2137d5c3e95d31f
maysha29 <mayshatabassum29@gmail.com>
2025-10-06 02:03:02 +0600

    Comprehension part is added

---
+++ com.py
@@ -0,0 +1,7 @@
+#Comprehension 
+#In python,list Comprehension means a shortcut for making a list.
+# Here no need to write long line codes.
+
+#for example : [x for x in range(5)]
+#output:0,1,2,3,4
+
