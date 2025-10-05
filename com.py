5a5a3186607db63e94cd09c5fe98903175febd27
maysha29 <mayshatabassum29@gmail.com>
2025-10-06 02:09:10 +0600

    next part is added

--- com.py
+++ com.py
@@ -5,3 +5,14 @@
 #for example : [x for x in range(5)]
 #output:0,1,2,3,4
 
+#(without a comprehension)
+squares=[]
+for x in range(1,6):
+    squares.append(x**2) 
+    print(squares)
+
+
+#(with a comprehension)
+squares=[x**2 for x in range(1,6)]
+print(squares)
+
